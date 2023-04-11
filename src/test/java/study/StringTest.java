5080b86d5a86024f5016b3470da4d6ef07794871
ckdtls1124 <ckdtls1124@naver.com>
2023-04-11 23:22:56 +0900

    0411

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -3,6 +3,7 @@ package study;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
     @Test
@@ -10,4 +11,42 @@ public class StringTest {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+
+    @Test
+    void splitTest1(){
+        String[] actual = "1,2".split(",");
+        assertThat(actual[0]).contains("1");
+    }
+
+    @Test
+    void splitTest2(){
+        String[] actual = "1,2".split(",");
+        assertThat(actual[1]).contains("2");
+    }
+
+    @Test
+    void splitTest3(){
+        String[] actual = "1".split(",");
+        assertThat(actual).containsExactly("1");
+    }
+
+    @Test
+    void splitTest4(){
+        String actual ="(1,2)".substring(1,4);
+        System.out.println(actual);
+    }
+
+    @Test
+    void charAtTest(){
+        char actual = "abc".charAt(2);
+    }
+
+    @Test
+    void charAtTestError(){
+        assertThatThrownBy(()->{
+            throw new IndexOutOfBoundsException("Wrong");
+        }).isInstanceOf(IndexOutOfBoundsException.class)
+                .hasMessageContaining("Wrong");
+    }
+
 }
