6ff682c0a029dfe9719a8bb22d8dd666c2712bed
ckdtls1124 <ckdtls1124@naver.com>
2023-05-09 12:41:55 +0900

    0509

--- src/main/java/numberbaseball/RandomNumberGenerator.java
+++ src/main/java/numberbaseball/RandomNumberGenerator.java
@@ -2,18 +2,13 @@ package numberbaseball;
 
 public class RandomNumberGenerator {
 
-    public static int numberGenerator(){
+    public static int numberGenerator() {
 
-        double ranNum = Math.random()*1000;
-
-        int threeDigitRandomNum=0;
+        double ranNum = Math.random() * 1000;
+        int threeDigitRandomNum = 0;
         if(ranNum >= 100){
-            double flooredNum = Math.floor(ranNum);
-
-            threeDigitRandomNum = (int) flooredNum;
-
+            threeDigitRandomNum = (int) Math.floor(ranNum);
         }
-
         return threeDigitRandomNum;
     }
 
